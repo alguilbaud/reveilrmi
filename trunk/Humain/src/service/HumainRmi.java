e11f93cd4a588cf9a2268c1c89fd863e1e4f479b
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@19f7ca82-0897-0eab-3a09-eec9af773aab>
2014-11-24 22:11:57 +0000

    ajout sonnerie (humain)

--- trunk/Humain/src/service/HumainRmi.java
+++ trunk/Humain/src/service/HumainRmi.java
@@ -4,15 +4,21 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
 public class HumainRmi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          try {
-			IReveil  stub= (IReveil)Naming.lookup("rmi://localhost:1099/reveil");
-			stub.armee();
-			stub.deSarmee();
+//			IReveil  stub= (IReveil)Naming.lookup("rmi://localhost:1099/reveil");
+//			stub.armee();
+//			stub.deSarmee();
+//			
+			LocateRegistry.createRegistry(1098);
+		    Humain humain= new Humain();
+		    Naming.rebind("rmi://localhost:1098/humain", humain);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
