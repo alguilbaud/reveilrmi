43dff555e24de4eee8f0eaefcaede9bad1811f85
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@19f7ca82-0897-0eab-3a09-eec9af773aab>
2014-11-24 22:12:14 +0000

    ajout sonnerie (reveil)

--- trunk/Reveil/src/service/ReveilRmi.java
+++ trunk/Reveil/src/service/ReveilRmi.java
@@ -10,13 +10,14 @@ public class ReveilRmi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        try {
-    	    LocateRegistry.createRegistry(1099);
-		    Reveil reveil= new Reveil();
-		    Naming.rebind("rmi://localhost:1099/reveil", reveil);
-	} catch (RemoteException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (MalformedURLException e) {
+//    	    LocateRegistry.createRegistry(1099);
+//		    Reveil reveil= new Reveil();
+//		    Naming.rebind("rmi://localhost:1099/reveil", reveil);
+		    IHumain  stub= (IHumain)Naming.lookup("rmi://localhost:1098/humain");
+			stub.entendSonnerie();
+			
+			
+	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
