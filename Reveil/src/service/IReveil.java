9029c5d0710bc60ed9b6deee5565dd727011268e
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@19f7ca82-0897-0eab-3a09-eec9af773aab>
2014-11-27 09:08:07 +0000

    passage sous linux (reveil)

--- Reveil/src/service/IReveil.java
+++ Reveil/src/service/IReveil.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
 public interface IReveil extends Remote {
 	
 	public void armee() throws RemoteException;
-	public void deSarmee() throws RemoteException;
+	public void desarmee() throws RemoteException;
 	
 }
